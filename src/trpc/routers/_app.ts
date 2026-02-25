00b8d6c3658acd7e1263b4407995c41dcd11b62f
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2026-02-25 20:37:40 +0700

    06: my tasks new1

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -14,12 +14,9 @@ export const appRouter = createTRPCRouter({
                 greeting: `hello ${input.text} ${ctx.user.name}`,
             };
         }),
-<<<<<<< Updated upstream
-=======
     projects: ProjectRouter,
     tasks: TaskRouter,
 
->>>>>>> Stashed changes
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
