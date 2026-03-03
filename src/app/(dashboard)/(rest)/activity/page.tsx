d0493c25f3372e33c248657464251107dd62f312
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2026-03-03 20:07:35 +0700

    07: activityfeed setup

---
+++ src/app/(dashboard)/(rest)/activity/page.tsx
@@ -0,0 +1,10 @@
+import { ActivityContainer } from "@/app/features/activity/components/activity"
+
+const ActivityPage = () => {
+    return (
+        <ActivityContainer>
+            <></>
+        </ActivityContainer>
+    )
+}
+export default ActivityPage
